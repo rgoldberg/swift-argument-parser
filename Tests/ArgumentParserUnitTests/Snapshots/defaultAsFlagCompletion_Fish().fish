e50e9f6a51812e2d94f247e7b55eed8525073e85
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2026-09-06 07:36:30 -0400

    FIX tests
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Tests/ArgumentParserUnitTests/Snapshots/defaultAsFlagCompletion_Fish().fish
+++ Tests/ArgumentParserUnitTests/Snapshots/defaultAsFlagCompletion_Fish().fish
@@ -30,7 +30,7 @@ end
 
 function __defaultasflag-test_tokens
     if test (string split -m 1 -f 1 -- . "$FISH_VERSION") -gt 3
-        commandline --tokens-raw $argv
+        commandline --tokens-raw $argv | string split0
     else
         commandline -o $argv
     end
@@ -80,7 +80,7 @@ complete -c 'defaultasflag-test' -f
 complete -c 'defaultasflag-test' -n '__defaultasflag-test_should_offer_completions_for_flags_or_options "defaultasflag-test" bin-path' -l 'bin-path' -rfa '(__defaultasflag-test_complete_directories)'
 complete -c 'defaultasflag-test' -n '__defaultasflag-test_should_offer_completions_for_flags_or_options "defaultasflag-test" count' -l 'count' -rfka ''
 complete -c 'defaultasflag-test' -n '__defaultasflag-test_should_offer_completions_for_flags_or_options "defaultasflag-test" verbose' -l 'verbose' -rfka ''
-complete -c 'defaultasflag-test' -n '__defaultasflag-test_should_offer_completions_for_flags_or_options "defaultasflag-test" log-level' -l 'log-level' -rfka 'DEBUG INFO WARN ERROR'
+complete -c 'defaultasflag-test' -n '__defaultasflag-test_should_offer_completions_for_flags_or_options "defaultasflag-test" log-level' -l 'log-level' -rfka '\'DEBUG\' \'INFO\' \'WARN\' \'ERROR\''
 complete -c 'defaultasflag-test' -n '__defaultasflag-test_should_offer_completions_for_flags_or_options "defaultasflag-test" help' -l 'help'
 complete -c 'defaultasflag-test' -n '__defaultasflag-test_should_offer_completions_for_positional "defaultasflag-test" -eq 1' -F
 complete -c 'defaultasflag-test' -n '__defaultasflag-test_should_offer_completions_for_flags_or_options "defaultasflag-test" h help' -s 'h' -l 'help' -d 'Show help information.'
